9c305efb6623e318ccfca52171d9e13f39f2ac1a
EPM Springfield <epmspringfield@EPMs-Mac.local>
2015-08-17 17:17:03 -0700

    Initial Commit

---
+++ WebApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebApp
+//
+//  Created by EPM Springfield on 8/17/15.
+//  Copyright (c) 2015 EPM Springfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebApp
+//
+//  Created by EPM Springfield on 8/17/15.
+//  Copyright (c) 2015 EPM Springfield. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
